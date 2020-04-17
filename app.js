6e85b9fd9b72af925e170b1261fa7cb681b349e5
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-04-16 17:47:57 -0700

    added loops

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
 'use strict'
+
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
+var trueFan = 'nope'
 
 function welcome() {
-    alert('Welcome to my website!') 
+    alert('Welcome to the Shire!') 
 }
+welcome();
+
+var userName = prompt('What is your name?')
 
 function morningOrAfternoon(){
-    if (hourNow > 10) {
+    if (hourNow > 12 ) {
         greeting = 'Good afternoon';
     } else if (hourNow > 0 ) {
         greeting = 'Good morning';
     } else {
         greeting = 'Welcome';
     }
-    document.write('<h3>'+ greeting + ' ' + userName + '!' + '</h3>');
+    document.write('<h2>'+ greeting + ' ' + userName + '!' + '</h2>');
     return greeting ;
 }
-welcome();
 
-var userName = prompt('What is your name?')
+while( (trueFan != 'yes') && (trueFan != 'no') ){
+    trueFan = prompt('Are you a true fan? (yes or no)').toLowerCase();
+}
+
+if(trueFan === 'no'){
+    alert('You are at the right place to learn about Lord of the Rings. Enjoy the journey into Middle Earth!');
+} else {
+    guessRings();
+}
+
+function guessRings() {
+
+    var correctAnswer = 20;
+
+    for(var i = 0; i < 3; i++){
+        var numberGuess = prompt('How many Rings of Power are there?');
+        if(numberGuess == correctAnswer){
+            alert('You are a true fan, enjoy the jouney into Middle Earth');
+            break;
+        } else {
+            alert('Sorry, please try again!');
+        }
+        if (i == '2') {
+            alert('You should rewatch the trilogy');
+
+        }
+    }
+}
+
